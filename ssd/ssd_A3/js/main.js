ef83cc7a4fdfbc8bdaf2e53a68c292ef7d8154fe
avushek2305 <55002577+avushek2305@users.noreply.github.com>
2022-10-06 18:34:15 +0530

    Add files via upload

--- ssd/ssd_A3/js/main.js
+++ ssd/ssd_A3/js/main.js
@@ -1,3 +1,15 @@
+
+const occ = new Map();
+for(let i = 0; i<10; i++){
+  occ.set("d"+toString(i+1),0);
+}
+
+
+
+//if occ[i] = 0 unoccupied
+//if occ[i] = 1 occupied by vulture 
+//if occ[i] = 2 occupied by 
+
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -9,5 +21,10 @@ function allowDrop(ev) {
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("circle");
-    ev.target.appendChild(document.getElementById(data));
+    if(occ.has(data) && occ.get(data) == 0)
+    {
+      ev.target.appendChild(document.getElementById(data));
+      
+      
+    }
   }
